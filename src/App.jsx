706675fef983e91ee77cf03ba967c0ebb7308744
Dobbymin <gmin990624@gmail.com>
2025-02-20 23:18:39 +0900

    feat: query 설정 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { globalStyle } from '@/shared';
+import { globalStyle, queryClient } from '@/shared';
 
 import { Routes } from './app/routes';
 import { Global } from '@emotion/react';
+import { QueryClientProvider } from '@tanstack/react-query';
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Global styles={globalStyle} />
       <Routes />
-    </>
+    </QueryClientProvider>
   );
 };
 
